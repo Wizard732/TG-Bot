6c6f1ca9b42d43f856d57786a5162b7fa41b69a6
Wizard732 <bvdovenko567@gmail.com>
2026-03-08 11:05:04 +0200

    добавил команду weight которая получает данные и вносит в таблицу

--- OmniAssistant-Bot_SQL/main.py
+++ OmniAssistant-Bot_SQL/main.py
@@ -6,15 +6,17 @@ from youtubesearchpython import VideosSearch
 import sqlite3
 import os
 from dotenv import load_dotenv
+from aiogram.filters import Command, CommandObject
+from datetime import date, datetime
 
 load_dotenv()
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 
 bot = Bot(token=BOT_TOKEN)
-dp = Dispatcher()
+dp = Dispatcher() # подключаем бота
 
 connect = sqlite3.connect('main.db')
-cursor = connect.cursor()
+cursor = connect.cursor() # подключаем sql
 
 # Создаем таблицы
 cursor.execute("""CREATE TABLE IF NOT EXISTS users(
@@ -30,6 +32,12 @@ cursor.execute("""CREATE TABLE IF NOT EXISTS music(
 )""")
 connect.commit()
 
+cursor.execute("""CREATE TABLE IF NOT EXISTS weight(
+    data INTEGER,
+    weight FLOAT
+)""")
+connect.commit()
+
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
     user_id = message.from_user.id
@@ -40,7 +48,7 @@ async def cmd_start(message: types.Message):
         VALUES (?, ?, 1)
         ON CONFLICT(id) DO UPDATE SET requests = requests + 1
     """, (user_id, user_name))
-    connect.commit()
+    connect.commit() # добавляем айди, ник, и количество запросов
 
     cursor.execute("SELECT requests FROM users WHERE id = ?", (user_id,))
     user_count = cursor.fetchone()[0]
@@ -55,12 +63,12 @@ async def search_and_send(message: types.Message, query: str):
     status_msg = await message.answer(f'🔍 ищу "{query}"...')
     try:
         search = VideosSearch(query, limit=1)
-        result = search.result()
+        result = search.result() # делаем поиск по названию и выводим самое похожее 
 
         if result['result']:
             video = result['result'][0]
             title = video['title']
-            link = video['link']
+            link = video['link'] # берем данные с json
             duration = video['duration']
 
             builder = InlineKeyboardBuilder()
@@ -68,7 +76,7 @@ async def search_and_send(message: types.Message, query: str):
             builder.row(
                 types.InlineKeyboardButton(text='🎧 Слушать', url=link),
                 types.InlineKeyboardButton(text='💾 Сохранить', callback_data='save_track')
-            )
+            ) # создаем кнопки 
             
             await status_msg.edit_text(
                 f"✅ Найдено!\n\n🎵 {title}\n⏱ {duration}",
@@ -97,7 +105,7 @@ async def save_to_db(callback: types.CallbackQuery):
 
         cursor.execute("INSERT INTO music (user_id, title, link) VALUES (?, ?, ?)", 
                        (user_id, track_title, track_link))
-        connect.commit()
+        connect.commit() # добавляем музыку в плейлист
 
         await callback.answer("✅ Сохранено!")
         await callback.message.answer(f"Трек «{track_title}» сохранен. Глянь /check")
@@ -117,7 +125,7 @@ async def cmd_check(message: types.Message):
 
     text = "📂 **Твои сохраненные треки:**\n\n"
     for i, row in enumerate(rows, 1):
-        text += f"{i}. [{row[0]}]({row[1]})\n"
+        text += f"{i}. [{row[0]}]({row[1]})\n" # выводим в более адекватном формате 
     
     await message.answer(text, parse_mode="Markdown", disable_web_page_preview=True)
 
@@ -126,6 +134,31 @@ async def cmd_check(message: types.Message):
 async def echo_search(message: types.Message):
     # При вводе текста вызываем ПОИСК, а не сохранение
     await search_and_send(message, message.text)
+
+
+@dp.message(Command('weight')) # Создаем команду weight
+async def cmd_weight(message:types.Message, command: CommandObject):
+
+    cmd = await message.answer('Впишите свой вес!') # просим пользователя вписать свой вес 
+    today = date.today() # получаем текущую дату
+
+    if command.args:
+        weight = command.args.strip() # command.args будет равен весу
+        await cmd.edit_text(f'Ваш вес {weight}кг успешно сохранен!')
+    
+    else:
+        await message.answer("Пожалуйста, введите вес после команды.")
+
+    cursor.execute("INSERT INTO weight (data, weight) VALUES (?,?)",
+                   (today,weight)) # Вводим данные в таблицу
+    
+    cursor.execute("SELECT * FROM weight")
+    rows = cursor.fetchall() # получаем данные с таблицы
+    for row in rows:
+        text = f'дата - {row[0]} \n вес - {row[1]}'
+
+    await message.answer(f'Ваши записанные данные;\n {text}') # выводим данные пользователю
+        
 
 
 async def main():
