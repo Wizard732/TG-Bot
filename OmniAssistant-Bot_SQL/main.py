cf06b3cc18ca52a680b5a8ff351cdd4624fadbe0
Wizard732 <bvdovenko567@gmail.com>
2026-03-15 18:05:49 +0200

    update: aded command help

--- OmniAssistant-Bot_SQL/main.py
+++ OmniAssistant-Bot_SQL/main.py
@@ -242,6 +242,41 @@ async def callback_info(callback: types.CallbackQuery,):
 
     await callback.answer()
 
+@dp.message(Command('help')) # создаем команду help
+async def cmd_help(message:types.Message):
+
+    button1 = types.InlineKeyboardButton(text="🛠 Список команд", callback_data="command")
+    keyboard1 = types.InlineKeyboardMarkup(inline_keyboard=[[button1]]) # создаю кнопку для списка команд 
+
+    await message.answer(
+        'Раздел помощь.',
+        reply_markup=keyboard1 # выводим кнопку
+    )
+
+@dp.callback_query(F.data == 'command') # если кнопка command была нажата выводим эти кнопки
+async def callback(callback:types.CallbackQuery):
+    keyboard = types.InlineKeyboardMarkup(inline_keyboard=
+        [[types.InlineKeyboardButton(text="/Start", callback_data="start")],
+        [types.InlineKeyboardButton(text="/Weight", callback_data="weight")], # вложенные кнопки
+        [types.InlineKeyboardButton(text="/Check", callback_data="check")]])
+
+
+    await callback.message.edit_text("Выберите команду:",
+         reply_markup=keyboard # выводим кнопки
+         )
+
+@dp.callback_query(F.data == 'start') # если кнопка start была нажата выводим эту информацию о ней
+async def callback(callback:types.CallbackQuery): 
+    await callback.message.answer('Команда (start) используется для\n записи данных пользователя в базу данных.')
+    
+@dp.callback_query(F.data == 'weight') # если кнопка start была нажата выводим эту информацию о ней
+async def callback(callback:types.CallbackQuery): 
+    await callback.message.answer('Команда (weight) ипользуется для\n записи веса пользователя, а так-же выводит его прогресс.')
+
+@dp.callback_query(F.data == 'check') # если кнопка start была нажата выводим эту информацию о ней
+async def callback(callback:types.CallbackQuery): 
+    await callback.message.answer('Команда (check) используется для\n добавления музыки в плейлист и удаления ее оттуда.')
+
 class Start():
     async def main():
         print("Бот запущен!")
